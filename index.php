a91381bf7a39d7c2d36db95e83b9b581efa4ef1b
Jhonathan Moreira <jhongmoreira@gmail.com>
2020-07-20 10:03:28 -0300

    Cabecalho do site

---
+++ index.php
@@ -0,0 +1,18 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Tudor - To do List</title>
+        <!--CSS Principal-->
+        <link rel="stylesheet" href="principal.css">
+        <!--CSS Boostrap-->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <!--CSS Fontawesome-->
+        <link rel="stylesheet" href="bootstrap/fontawesome/css/fontawesome.css">
+        <link rel="stylesheet" href="bootstrap/fontawesome/css/brands.css">
+        <link rel="stylesheet" href="bootstrap/fontawesome/css/regular.css">
+        <link rel="stylesheet" href="bootstrap/fontawesome/css/solid.css">
+        <!-- jQuery e JS-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+    </head>
+</html>
